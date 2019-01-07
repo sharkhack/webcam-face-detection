ab0f26ade1c439d6286861ba975b46bc4a752af7
Azer Bulbul <sharkhack@gmail.com>
2019-01-06 22:02:06 -0800

    Add files via upload

---
+++ webcam_face_1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import cv2
+import sys
+
+cascPath = "./data/haarcascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags= cv2.CASCADE_SCALE_IMAGE
+    )
+
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey (1) == 13: #13 is the Enter Key
+        break
+
+
+video_capture.release()
+cv2.destroyAllWindows()
